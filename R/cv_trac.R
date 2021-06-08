d4cf8d5d6e6c01d57bd8f0129d81a16cedc2267d
vttr <viet.tran@campus.lmu.de>
2021-06-08 05:58:17 +0200

    Rebase master into fork

--- R/cv_trac.R
+++ R/cv_trac.R
@@ -4,19 +4,22 @@
 #' \code{nfold}-fold cross validation.
 #'
 #' @param fit output of \code{\link{trac}} function.
-#' @param Z,y,A same arguments as passed to \code{\link{trac}}
+#' @param Z,y,A,X same arguments as passed to \code{\link{trac}}
 #' @param folds a partition of \code{1:nrow(Z)}.
 #' @param nfolds number of folds for cross-validation
 #' @param summary_function how to combine the errors calculated on each
 #' observation within a fold (e.g. mean or median)
 #' @export
-cv_trac <- function(fit, Z, y, A, folds = NULL, nfolds = 5, summary_function = stats::median) {
+cv_trac <- function(fit, Z, y, A, X = NULL, folds = NULL, nfolds = 5,
+                    summary_function = stats::median) {
   n <- nrow(Z)
   p <- ncol(Z)
   stopifnot(length(y) == n)
-  if(is.null(folds)) folds <- ggb:::make_folds(n, nfolds)
-  else
+  if (is.null(folds)) {
+    folds <- ggb:::make_folds(n, nfolds)
+  } else {
     nfolds <- length(folds)
+  }
   cv <- list()
   fit_folds <- list() # save this to reuse by log-ratio's cv function
   for (iw in seq_along(fit)) {
@@ -26,28 +29,69 @@ cv_trac <- function(fit, Z, y, A, folds = NULL, nfolds = 5, summary_function = s
       cat("fold", i, fill = TRUE)
       # train on all but i-th fold (and use settings from fit):
       fit_folds[[i]] <- trac(Z[-folds[[i]], ],
-                      y[-folds[[i]]],
-                      A, fraclist = fit[[iw]]$fraclist, w = fit[[iw]]$w)
+        y[-folds[[i]]],
+        A,
+        X[-folds[[i]]],
+        fraclist = fit[[iw]]$fraclist,
+        w = fit[[iw]]$w,
+        method = fit[[iw]]$method
+      )
       if (fit[[iw]]$refit) {
-        fit_folds[[i]] <- refit_trac(fit_folds[[i]], Z[-folds[[i]], ],
-                              y[-folds[[i]]], A)
+        fit_folds[[i]] <- refit_trac(
+          fit_folds[[i]], Z[-folds[[i]], ],
+          y[-folds[[i]]], A
+        )
       }
-      errs[, i] <- apply((predict_trac(fit_folds[[i]],
-                                         Z[folds[[i]], ])[[1]] - y[folds[[i]]])^2, 2, summary_function)
+      if (fit[[iw]]$method == "regression") {
+        errs[, i] <- apply(
+          (predict_trac(
+            fit_folds[[i]],
+            Z[folds[[i]], ],
+            X[folds[[i]], ])[[1]] - y[folds[[i]]])^2,
+          2, summary_function
+        )
+      }
+      if (fit[[iw]]$method == "classification") {
+        # loss: max(0, 1 - y_hat * y)^2
+        r <- 1 - predict_trac(
+          fit_folds[[i]], Z[folds[[i]], ], X[folds[[i]], ])[[1]] * y[folds[[i]]]
+        r_max <- r > 0
+        r <- (r * r_max)^2
+        errs[, i] <- apply(r, 2, summary_function)
+      }
+      if (fit[[iw]]$method == "classification_huber") {
+        rho_classification <- fit[[iw]]$rho_classification
+        r <- predict_trac(
+          fit_folds[[i]], Z[folds[[i]], ], X[folds[[i]], ])[[1]]* y[folds[[i]]]
+        r_case_3 <- !(r >= 1)
+        r_case_2 <- r <= rho_classification
+        r_case_1 <- (r > rho_classification) & (r < 1)
+
+        r <- r_case_3 * r
+        r[r_case_2] <- (1 - rho_classification) *
+          (1 + rho_classification - 2 * r[r_case_2])
+        r[r_case_1] <- (1 - r[r_case_1])^2
+
+        errs[, i] <- apply(r, 2, summary_function)
+      }
     }
     m <- rowMeans(errs)
     se <- apply(errs, 1, stats::sd) / sqrt(nfolds)
     ibest <- which.min(m)
     i1se <- min(which(m < m[ibest] + se[ibest]))
-    cv[[iw]] <- list(errs = errs, m = m, se = se,
-                     lambda_best = fit[[iw]]$fraclist[ibest], ibest = ibest,
-                     lambda_1se = fit[[iw]]$fraclist[i1se], i1se = i1se,
-                     fraclist = fit[[iw]]$fraclist, w = fit[[iw]]$w,
-                     nonzeros = colSums(abs(fit[[iw]]$gamma) > 1e-5),
-                     fit_folds = fit_folds)
+    cv[[iw]] <- list(
+      errs = errs, m = m, se = se,
+      lambda_best = fit[[iw]]$fraclist[ibest], ibest = ibest,
+      lambda_1se = fit[[iw]]$fraclist[i1se], i1se = i1se,
+      fraclist = fit[[iw]]$fraclist, w = fit[[iw]]$w,
+      nonzeros = colSums(abs(fit[[iw]]$gamma) > 1e-5),
+      fit_folds = fit_folds
+    )
   }
-  list(cv = cv,
-       iw_best = which.min(lapply(cv, function(cvv) cvv$m[cvv$ibest])),
-       iw_1se = which.min(lapply(cv, function(cvv) cvv$m[cvv$i1se])),
-       folds = folds)
+  list(
+    cv = cv,
+    iw_best = which.min(lapply(cv, function(cvv) cvv$m[cvv$ibest])),
+    iw_1se = which.min(lapply(cv, function(cvv) cvv$m[cvv$i1se])),
+    folds = folds
+  )
 }
--- R/predict.R
+++ R/predict.R
@@ -2,18 +2,31 @@
 #'
 #' @param fit output of the function \code{\link{trac}}
 #' @param new_Z a new data matrix (see \code{Z} from \code{\link{trac}})
-#'
+#' @param new_X a new data matrix (see \code{X} from \code{\link{trac}})
 #' @return a vector of \code{nrow(new_Z)} predictions.
 #' @export
-predict_trac <- function(fit, new_Z) {
+predict_trac <- function(fit, new_Z, new_X = NULL) {
   # fit: output of wag
   # new_Z: n_new by p matrix
+  classification <- fit[[1]]$method %in% c("classificiation",
+                                           "classification_huber")
+  intercept_classif <- fit[[1]]$intercept_classif
   yhat <- list()
   for (iw in seq_along(fit)) {
-    yhat[[iw]] <- t(as.numeric(fit[[iw]]$beta0) + t(as.matrix(new_Z %*% fit[[iw]]$beta)))
+    if (!is.null(new_X)) {
+      new_Z <- cbind(new_Z, new_X)
+    }
+    if (intercept_classif) {
+      yhat[[iw]] <- t(as.numeric(fit[[iw]]$beta0) +
+        t(as.matrix(new_Z %*% fit[[iw]]$beta)))
+    } else {
+      yhat[[iw]] <- t(t(as.matrix(new_Z %*% fit[[iw]]$beta)))
+    }
+#    if (classification) {
+#      yhat[[iw]] <- yhat[[iw]] >= 0
+#      yhat[[iw]] <- yhat[[iw]] * 2 - 1
+#    }
     rownames(yhat[[iw]]) <- rownames(new_Z)
   }
   return(yhat)
 }
-
-
